0c77d7b5faeae194f2c2dc98decab5bb0a33e7e1
Loïs <lois.puig@kctus.fr>
2015-07-09 16:58:44 +0200

    no message

--- osmanage.sh
+++ osmanage.sh
@@ -3,6 +3,15 @@
 set -e
 clear
 
+while getopts pp: option
+do
+	case "${option}"
+	in
+	p) PURGE=true;;
+	esac
+done
+
+
 #*************************************************************************
 
 OSM2PGSQL_BIN=/usr/local/share/osm2pgsql/osm2pgsql
@@ -31,19 +40,25 @@ while true; do
 		echo ""
 		echo "---"
 
-		echo "Name: "$DATA_NAME
-		echo "File: "$DATA_FILE
-		echo "PBF URL: "$DATA_URL
+		echo "Name      : "$DATA_NAME
+		echo "File      : "$DATA_FILE
+		echo "PBF URL   : "$DATA_URL
 		echo "Update URL: " $DATA_UPDATE_URL
 
 		echo "---"
+
+		if [ $PURGE ]; then
+			sudo -u postgres psql -d gis --command "DROP SCHEMA public CASCADE; CREATE SCHEMA public; GRANT ALL ON SCHEMA public TO postgres; GRANT ALL ON SCHEMA public TO public; COMMENT ON SCHEMA public IS 'standard public schema';"
+			rm $DATA_DIR/$DATA_FILE.osm.pbf $DATA_CHANGES $EXPIRED_TILES_LIST
+			rm -rf $TILES_DIR/*
+		fi
 
 		if [ ! -e "$DATA_DIR/$DATA_FILE.osm.pbf" ]; then
 			echo "OSM data file not found, starting download..."
 			wget -O $DATA_DIR/$DATA_FILE.osm.pbf $DATA_URL
 			
 			echo "Importing $DATA_NAME to database"
-			sudo -u www-data $OSM2PGSQL_BIN $OSM2PGSQL_OPTIONS --expire-tiles 0 --expire-output $EXPIRED_TILES_LIST $DATA_DIR/$DATA_FILE.osm.pbf
+			sudo -u www-data $OSM2PGSQL_BIN $OSM2PGSQL_OPTIONS --expire-tiles 0 --expire-output $EXPIRED_TILES_LIST $APPEND $DATA_DIR/$DATA_FILE.osm.pbf
 		else
 			echo "OSM data file found"
 			
